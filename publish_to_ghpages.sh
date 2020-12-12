8ae3049c0f3c86b6498088edd78539984f0253b2
Dmitry Balabka <dmitry.balabka@gmail.com>
2020-12-12 20:35:18 +0200

    Adjust publish script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -25,5 +25,5 @@ echo "dmitry.balabka.com" > public/CNAME
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
